b8e1bfd92a150d35f38d3106424b78947d0e365d
MushroomyJr <mustafa.the.almighty@gmail.com>
2021-10-14 10:40:34 -0400

    more thorough forecast added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,12 +12,16 @@ const forecast = (lat, lon, callback) =>{
             const current = body.current
             const description = current.weather[0].description
             const hourly = body.hourly[0]
+            const daily = body.daily[0]
+            const high = daily.temp.max
+            const low = daily.temp.min
+
             data= {
                 temp: current.temp,
                 feelsLike: current.feels_like,
                 description: description,
                 pop: hourly.pop,
-                forecast:('Temperature of '+current.temp+', feels like '+current.feels_like+', '+description+'. There is a '+(hourly.pop*100)+'% chance of precipitation')
+                forecast:('Temperature of '+current.temp+', feels like '+current.feels_like+', '+description+'. There is a '+(hourly.pop*100)+'% chance of precipitation. The daily high is '+high+', and the low is '+low)
             }
             callback(undefined, data)
         } 
